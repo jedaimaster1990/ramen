afe420cbd919ff3a60b7cc99751983c5464b8386
Ubuntu <jedaimaster1990>
2025-11-03 08:51:25 +0000

    Initial Commit

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,35 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      flash[:notice] = "登録しました"
+      redirect_to post_images_path
+    else
+      flash.now[:alert] = "失敗しました"
+      render :new
+    end
+  end
+
+  def index
+    @post_images = PostImage.page(params[:page])
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+end
